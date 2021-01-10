0eef83a87a20f98b4cbf21e64849d5bcd09dbc63
jishu <jishuya3015@gmail.com>
2021-01-10 22:04:32 +0900

    Create project

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    {{ todoList }}
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <List/>
+      </v-flex>
+      <v-flex>
+        <ListAdd @listAdd="listAdd" />
+      </v-flex>
+    </v-layout>
+</template>
+<script>
+import List from "./List"
+import ListAdd from "./ListAdd"
+
+export default {
+  components: {
+    List,
+    ListAdd
+  },
+  data() {
+    return{
+      todoList: []
+    }
+  },
+  methods: {
+    listAdd(memo) {
+      console.log("받았어!!")
+      this.todoList.push({  })
+    }
+  }  
+}
+</script>
